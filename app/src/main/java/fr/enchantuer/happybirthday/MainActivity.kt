acde9c1abbb0d0b1a26ee474e017669eb812978f
enchantuer <florentbutery@gmail.com>
2025-01-22 23:17:56 -0500

    Exo 2 : Step 7
    Adopt good code practices

--- app/src/main/java/fr/enchantuer/happybirthday/MainActivity.kt
+++ app/src/main/java/fr/enchantuer/happybirthday/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,8 +31,8 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     GreetingImage(
-                        message = "Happy Birthday Florent!",
-                        from = "From Lucas",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
                     )
                 }
             }
@@ -85,6 +86,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Florent!", from = "From Lucas")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
     }
 }
